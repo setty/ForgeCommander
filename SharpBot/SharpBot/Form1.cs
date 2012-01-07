906446b1602ee465c7309ab58f8f99e803d5ddc0
Wouter Gerarts <wouto1997@gmail.com>
2012-01-07 22:32:48 +0100

    Fixed SharpBot/SharpBot/Form1.cs

--- SharpBot/SharpBot/Form1.cs
+++ SharpBot/SharpBot/Form1.cs
@@ -21,6 +21,42 @@ namespace SharpBot
             SharpControl.Login(txtUser.Text, txtPass.Text, txtServer.Text);
             Hide();
             (new User_Control()).ShowDialog();
+            if (txtUser.Text != "" && txtServer.Text != "")
+            {
+                string port;
+                string sendserver;
+                if (txtPort.Text == "")
+                {
+                    port = "25565";
+                }
+                else
+                {
+                    port = txtPort.Text;
+                }
+                sendserver = txtServer.Text + ":" + port;
+                try
+                {
+                    SharpControl.Login(txtUser.Text, txtPass.Text, sendserver);
+                    Hide();
+                    (new User_Control()).ShowDialog();
+
+                }
+                catch
+                {
+                    MessageBox.Show("Could not enstablish a connection.");
+                }
+            }
+            else
+            {
+                if (txtUser.Text == "")
+                {
+                    MessageBox.Show("Make sure to enter an username!");
+                }
+                if (txtServer.Text == "")
+                {
+                    MessageBox.Show("Make sure to enter a hostname!");
+                }
+            }
         }
     }
 }
